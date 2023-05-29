f75ce721ad66bea5eb01e0c3ddb4c2f155f63cd1
BrandonTuan777 <tuansamdang@gmail.com>
2023-05-29 13:52:27 +0700

    Refactor code

--- Automation_Test/src/main/java/MainFlow/CreateNewPost.java
+++ Automation_Test/src/main/java/MainFlow/CreateNewPost.java
@@ -1,4 +1,4 @@
-package org.example;
+package MainFlow;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
@@ -9,7 +9,7 @@ import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-class DemoDACNTT1 {
+class CreateNewPost {
     ChromeDriver chromeDriver;
 
     @BeforeMethod
---
+++ Automation_Test/src/main/java/MainFlow/ReviewPost.java
@@ -0,0 +1,26 @@
+package MainFlow;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class ReviewPost {
+    ChromeDriver chromeDriver;
+    @BeforeMethod
+    public void Setup() {
+        WebDriverManager.chromedriver().setup();
+
+        ChromeOptions option = new ChromeOptions().addArguments("--remote-allow-origins=*");
+
+        chromeDriver = new ChromeDriver(option);
+
+        chromeDriver.get("https://rongbay.com/");
+    }
+
+    @Test
+    public void ReviewPost{
+        
+    }
+}
